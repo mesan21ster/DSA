f45786e4b9d2111c57d6d567fe70b345dbe0636b
Santosh Kumar <santosk@microsoft.com>
2023-04-02 23:31:02 +0530

    binary search

---
+++ Reducing Dishes/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ReducingDishes
+{
+    //Recursion + Memo : Knapsack
+    public class Solution
+    {
+        int[,] dp = new int[501, 501];
+        public int MaxSatisfaction(int[] satisfaction)
+        {
+            int n = satisfaction.Length;
+            Array.Sort(satisfaction);
+            for (int i = 0; i < 501; i++)
+            {
+                for (int j = 0; j < 501; j++)
+                {
+                    dp[i, j] = -1;
+                }
+            }
+            return solve(satisfaction, 0, 1, n);//0=index, 1= time
+        }
+
+        private int solve(int[] satisfaction, int i, int time, int n)
+        {
+            if (i >= n)
+            {
+                return 0;
+            }
+
+            if (dp[i, time] != -1)
+            {
+                return dp[i, time];
+            }
+
+            int include = satisfaction[i] * time + solve(satisfaction, i + 1, time + 1, n);//every recursion increase index and time
+            int exclude = solve(satisfaction, i + 1, time, n);//we are not including dishes this time so index will increase but time will not, because we have skiped dish so next dish will start at pervious time
+
+            return dp[i, time] = Math.Max(include, exclude);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] satisfaction = { -1, -8, 0, 5, -9 };
+            var res = s.MaxSatisfaction(satisfaction);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Successful Pairs of Spells and Potions/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace SuccessfulPairsofSpellsandPotions
+{
+    public class Solution
+    {
+        //Time O(nlongm)
+        //Space O(1)
+        public int[] SuccessfulPairs(int[] spells, int[] potions, long success)
+        {
+            int[] ans = new int[spells.Length];
+            Array.Sort(potions);
+
+            for (int i = 0; i < spells.Length; i++) // spells are fixed and loop to all
+            {
+                int low = 0;
+                int high = potions.Length - 1;
+                int successIndex = potions.Length;
+                while (low <= high) // for potions we can use binary search
+                {
+                    int mid = (low + high) / 2;
+                    long curr = spells[i] * potions[mid];
+
+                    if (curr >= success)
+                    {
+                        successIndex = mid; // get the success index , from this index to right all element would be success so we need to validate only left side so set high = mid - 1
+                        high = mid - 1;
+                    }
+                    else
+                    {
+                        low = mid + 1; // else find success towards right
+                    }
+                }
+                ans[i] = (potions.Length - successIndex); // all the element will be pair from success index
+            }
+            return ans;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] spells = { 5, 1, 3 }, potions = { 1, 2, 3, 4, 5 };
+            int success = 7;
+            var res = s.SuccessfulPairs(spells,potions,success);
+
+            foreach (int i in res)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
