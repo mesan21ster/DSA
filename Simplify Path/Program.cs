de3f53a2587be5b206b85cc64787cda5678eab3f
Santosh Kumar <santosk@microsoft.com>
2023-04-13 00:03:01 +0530

    Simplify Path

---
+++ Simplify Path/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace SimplifyPath
+{
+    public class Solution
+    {
+        //Time O(N)
+        //space O(N)
+        public string SimplifyPath(string path)
+        {
+            Stack<string> stack = new Stack<string>();
+            string[] paths = path.Split('/',StringSplitOptions.RemoveEmptyEntries);
+            foreach (var p in paths)
+            {
+                if(p == "/" || p== ".")
+                {
+                    continue;
+                }
+                else if(p == "..")
+                {
+                    if (stack.Any())
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    stack.Push(p);
+                }
+            }
+            string[] arr = stack.ToArray();
+            Array.Reverse(arr);
+            return "/" + string.Join("/", arr);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string path = "/home/";
+            Console.WriteLine(solution.SimplifyPath(path));
+            Console.ReadLine();
+        }
+    }
+}
