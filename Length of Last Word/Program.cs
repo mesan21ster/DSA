a0ed56c526353facbec60033896dad538b762512
Santosh Kumar <santosk@microsoft.com>
2023-04-17 22:47:32 +0530

    some more leet code solutions

---
+++ Length of Last Word/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LengthofLastWord
+{
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            string[] words = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            string lastword = words[words.Length - 1];
+
+            return lastword.Length;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            string s = "   fly me   to   the moon  ";
+            Console.WriteLine(solution.LengthOfLastWord(s));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Longest Common Prefix/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace LongestCommonPrefix
+{
+    public class Solution
+    {
+        //time complexity is O(Nlog(N) + M)
+        //Iterating over the characters of the first and last strings takes O(M) time. This is because the code compares the characters of the two strings until it finds the first mismatch.
+        public string LongestCommonPrefix(string[] strs)
+        {
+            Array.Sort(strs, StringComparer.OrdinalIgnoreCase);
+
+            string s1 = strs[0];
+            string s2 = strs[strs.Length -1];
+            string res = "";
+
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                if (s1[i] == s2[i])
+                {
+                    res += s1[i];
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return res;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            string[] strs = { "flower", "flow", "flight" };
+            Console.WriteLine(solution.LongestCommonPrefix(strs));
+            Console.ReadLine();
+        }
+    }
+}
