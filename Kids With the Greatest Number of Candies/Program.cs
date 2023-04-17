db93234e1793f9092aab8ec28c7d6bba2214a5e3
Santosh Kumar <santosk@microsoft.com>
2023-04-17 19:37:28 +0530

    Added few more leetcode solution

---
+++ Kids With the Greatest Number of Candies/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace KidsWiththeGreatestNumberofCandies
+{
+    //Time O(N)
+    //Space O(N)
+    public class Solution
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            List<bool> res = new List<bool>();
+            int m = candies.Length;
+            int max = candies.Max();
+
+            for (int i = 0; i < m; i++)
+            {
+                res.Add(false);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                if (candies[i] + extraCandies >= max)
+                {
+                    res[i] = true;
+                }
+            }
+            return res;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] candies = { 2, 3, 5, 1, 3 };
+            int extraCandies = 3;
+            var res = solution.KidsWithCandies(candies, extraCandies);
+            foreach (var item in res)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Maximum Value of K Coins From Piles/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace MaximumValueofKCoinsFromPiles
+{
+    public class Solution
+    {
+        int n = 0;
+        int[,] temp = new int[1001, 2001];
+        private int solve(int i, IList<IList<int>> piles, int k)
+        {
+            if (i >= n)
+            {
+                return 0;
+            }
+            if (temp[i, k] != -1)
+            {
+
+                return temp[i, k];
+            }
+            int not_taken = solve(i + 1, piles, k); //increase i to 1 because we are not considering i
+
+            int taken = 0;
+            int sum = 0;
+
+            for (int j = 0; j < Math.Min((int)piles[i].Count, k); j++)
+            {
+                sum += piles[i][j];
+                taken = Math.Max(taken, sum + solve(i + 1, piles, k - (j + 1))); //recursive call with next index to i and decrease k with j+1 //here j index start with 0 so + 1 means exact count
+            }
+            return temp[i, k] = Math.Max(taken, not_taken);//memoization
+        }
+        public int MaxValueOfCoins(IList<IList<int>> piles, int k)
+        {
+            n = piles.Count;
+            for (int i = 0; i < 1001; i++)
+            {
+                for (int j = 0; j < 2001; j++)
+                {
+                    temp[i, j] = -1;
+                }
+            }
+            return solve(0, piles, k);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[][] piles = {
+                new int[]{1, 100, 3 },
+                new int[]{7, 8, 9 }
+            };
+            int k = 2;
+            var res = solution.MaxValueOfCoins(piles, k);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
