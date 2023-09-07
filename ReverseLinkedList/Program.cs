d30f5bd4ca341a42fff6c2ace52f6bf77d99674e
Santosh Kumar <santosk@microsoft.com>
2023-09-07 23:38:19 +0530

    206. Reverse Linked List

---
+++ ReverseLinkedList/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ReverseLinkedList
+{
+
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode curr = head;
+
+            while (curr != null)
+            {
+                ListNode temp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = temp;
+            }
+            return prev;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            //head.next.next.next = new ListNode(4);
+           // head.next.next.next.next = new ListNode(5);
+
+            var res = solution.ReverseList(head);
+
+            while (res != null)
+            {
+                Console.WriteLine(res.val);
+                res = res.next;
+            }
+            Console.ReadLine();
+        }
+    }
+}
