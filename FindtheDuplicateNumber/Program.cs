61f4d86d2355fb9b8b08c4e3f221227b46e99787
Santosh Kumar <santosk@microsoft.com>
2023-09-19 20:30:14 +0530

    287. Find the Duplicate Number

---
+++ FindtheDuplicateNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace FindtheDuplicateNumber
+{
+    public class Solution
+    {
+        public int FindDuplicate(int[] nums)
+        {
+            int slow = nums[0];
+            int fast = nums[0];
+
+            slow = nums[slow];//move one
+            fast = nums[nums[fast]];//move twice
+
+            while(slow != fast)// this is to detect cycle
+            {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+
+            slow = nums[0];
+
+            while(fast != slow)
+            {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+            return slow;//we can slow return fast, both will be pointing to same value 
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] nums = { 3, 1, 3, 4, 2 };
+            var res = s.FindDuplicate(nums);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TheKWeakestRowsinaMatrix/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+
+namespace TheKWeakestRowsinaMatrix
+{
+    public class Solution
+    {
+        public int[] KWeakestRows(int[][] mat, int k)
+        {
+            int m = mat.Length;
+            int n = mat[0].Length;
+            Dictionary<int, int> countOnes = new Dictionary<int, int>();
+            for (int row = 0; row < m; row++)
+            {
+                int count_ofOnes = binarySearch(mat[row], 0, n - 1);
+                if (!countOnes.ContainsKey(row))
+                {
+                    countOnes.Add(row, count_ofOnes);
+                }
+                else
+                {
+                    int val  = countOnes[row];
+                    countOnes[row] = Math.Min(val, row);
+                    
+                }
+            }
+
+            Dictionary<int, int> sortedDic = countOnes.OrderBy(x => x.Value)
+             .ToDictionary(x => x.Key, x => x.Value);
+
+            int[] kList = new int[k];
+            int c = 0;
+            foreach (var item in sortedDic)
+            {
+                if (c >= k) break;
+
+                kList[c++] = item.Key;
+            }
+            return kList;
+
+
+        }
+        private int binarySearch(int[] mat, int l, int r)
+        {
+            int result = -1;
+
+            while (l <= r)
+            {
+                int mid = l + (r - l) / 2;
+                if (mat[mid] == 1)
+                {
+                    result = mid;
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+            return result + 1;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[][] mat =
+             {
+                new int[] { 1, 1, 0, 0, 0 },
+                new int[] { 1, 1, 1, 1, 0 },
+                new int[] { 1, 0, 0, 0, 0 },
+                new int[] { 1, 1, 0, 0, 0 },
+                new int[] { 1, 1, 1, 1, 1 }
+            };
+            int k = 3;
+
+            var res = solution.KWeakestRows(mat, k);
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
