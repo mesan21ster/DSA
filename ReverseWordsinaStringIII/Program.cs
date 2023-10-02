494e6aa61379b6f501eddd947f2cad2c32ddd8af
Santosh Kumar <santosk@microsoft.com>
2023-10-02 23:30:34 +0530

    557. Reverse Words in a String III

---
+++ ReverseWordsinaStringIII/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ReverseWordsinaStringIII
+{
+    public class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            string[] words = s.Split(' ');
+            string[] result = new string[words.Length];
+            for (int i = 0; i < words.Length; i++) {
+                char[] wordChar = words[i].ToCharArray();
+                Array.Reverse(wordChar);
+                result[i] = string.Join("", wordChar); 
+            }
+            return string.Join(" ", result);
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            string s = "God Ding";
+            var res = sol.ReverseWords(s);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
