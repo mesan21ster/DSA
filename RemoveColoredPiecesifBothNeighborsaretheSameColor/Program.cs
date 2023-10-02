0ec2ad7fd445e33f721d105e99487d2253a1ccd5
Santosh Kumar <santosk@microsoft.com>
2023-10-02 23:35:19 +0530

    2038. Remove Colored Pieces if Both Neighbors are the Same Color

---
+++ RemoveColoredPiecesifBothNeighborsaretheSameColor/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace RemoveColoredPiecesifBothNeighborsaretheSameColor
+{
+    public class Solution
+    {
+        public bool WinnerOfGame(string colors)
+        {
+            int n = colors.Length;
+            int alice = 0;
+            int bob = 0;
+            for (int i = 1; i < n - 1; i++)
+            {
+                if (colors[i - 1] == colors[i] && colors[i] == colors[i + 1])
+                {
+                    if (colors[i] == 'A')
+                    {
+                        alice++;
+                    }
+                    else
+                    {
+                        bob++;
+                    }
+                }
+            }
+            return alice> bob;
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            string colors = "ABBBBBBBAAA";
+            var res = sol.WinnerOfGame(colors);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
