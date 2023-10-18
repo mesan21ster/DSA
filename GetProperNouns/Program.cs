3205f08502526063c97e2fd8908d23cc97aacb86
Santosh Kumar <santosk@microsoft.com>
2023-10-18 23:09:15 +0530

    commmit

---
+++ GetProperNouns/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GetProperNouns
+{
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+           List<int> list = new List<string>(10);
+            Array.Fill(list, 0);
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ RemoveQuotesInsideSquareBracketsAsync/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace RemoveQuotesInsideSquareBracketsAsync
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static async Task Main()
+        {
+            string input = "EVALUATE CALCULATETABLE ( SELECTCOLUMNS ( Employee, \"EmployeeName\", Employee[\"Full Name\"], \"CostCenter\", Employee[\"Company Code\"] ), Employee[Email] = \"ajrai\" )";
+            string output = await RemoveQuotesInsideSquareBracketsAsync(input);
+            Console.WriteLine(output);
+        }
+
+        static async Task<string> RemoveQuotesInsideSquareBracketsAsync(string input)
+        {
+            return await Task.Run(() =>
+            {
+                string pattern = @"\[([^\]]*)\]";
+                MatchCollection matches = Regex.Matches(input, pattern);
+
+                foreach (Match match in matches)
+                {
+                    string insideBrackets = match.Groups[1].Value;
+                    string replacedInsideBrackets = insideBrackets.Replace("\"", string.Empty);
+                    string replacedBrackets = $"[{replacedInsideBrackets}]";
+                    input = input.Replace(match.Value, replacedBrackets);
+                }
+
+                return input;
+            });
+        }
+    }
+
+}
