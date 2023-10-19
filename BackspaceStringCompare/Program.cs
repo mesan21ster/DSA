cd6e2c456ec7f8f7f9fcb2210ad1a92781492117
Santosh Kumar <santosk@microsoft.com>
2023-10-19 21:31:22 +0530

    844. Backspace String Compare

---
+++ BackspaceStringCompare/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace BackspaceStringCompare
+{
+    public class Solution
+    {
+        public bool BackspaceCompare(string s, string t)
+        {
+            string temp1 = "";
+            string temp2 = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '#')
+                {
+                    if (temp1.Length > 0)
+                    {
+                        temp1 = temp1.Substring(0, temp1.Length - 1);
+                    }
+                }
+                else
+                {
+                    temp1 += s[i];
+                }
+            }
+
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i] == '#')
+                {
+                    if (temp2.Length > 0)
+                    {
+                        temp2 = temp2.Substring(0, temp2.Length - 1);
+                    }
+                }
+                else
+                {
+                    temp2 += t[i];
+                }
+            }
+
+            return string.Equals(temp1, temp2);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            var res = solution.BackspaceCompare("xywrrmp", "xywrrmu#p");
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
